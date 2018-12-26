9e6b4ec88e67daf300dd23179c511286f694764d
Scott Ryburn <sryburn@gmail.com>
2018-12-26 18:55:08 +1300

    initial countdown timer code

--- src/nextion-test.ino
+++ src/nextion-test.ino
@@ -1,25 +1,47 @@
 // This #include statement was automatically added by the Particle IDE.
 #include <ITEADLIB_Nextion.h>
-
+#include<string>
 USARTSerial& nexSerial = Serial1;
 
+SYSTEM_THREAD(ENABLED);
+
+Timer mockWaveform(500, generateWaveform);
+Timer countdownTimer(1000, updateCountdownTimer);
+
 int counter = 1;
+int decaminutes = 1;
+int minutes = 1;
+int decaseconds = 0;
+int seconds = 0;
 
 //declare stuff that we receive from display
-NexButton b0 = NexButton(0,2,"b0");
+NexButton boil = NexButton(0,22,"m0");
+NexButton mash = NexButton(0,23,"m1");
+NexButton hlt = NexButton(0,24,"m2");
+NexButton coil = NexButton(0,25,"m3"); //not used
+NexButton pump1 = NexButton(0,26,"m4");
+NexButton pump2 = NexButton(0,27,"m5");
 
+NexButton power = NexButton(0,6,"b0");
+NexButton restart = NexButton(0,8,"b1");
+NexButton countdown = NexButton(0,9,"b2");
+NexButton play = NexButton(0,10,"b3");
+NexButton settings = NexButton(0,11,"b4");
 
+NexWaveform chart = NexWaveform(0,21,"s0");
 NexTouch *nex_listen_list[] =
 {
-   &b0,
+   &boil,
+   &countdown,
    NULL
 };
 
-void b0PushCallback(void *ptr){
+void boilPushCallback(void *ptr){
     digitalWrite(D7, HIGH);
+    countdown.setText("60:00");
 }
 
-void b0PopCallback(void *ptr){
+void boilPopCallback(void *ptr){
     digitalWrite(D7, LOW);
 }
 
@@ -34,26 +56,60 @@ void setup() {
     Serial1.end();
     Serial1.begin(115200);
     
-    b0.attachPush(b0PushCallback);
-    b0.attachPop(b0PopCallback);
+    boil.attachPush(boilPushCallback);
+    boil.attachPop(boilPopCallback);
     pinMode(D7, OUTPUT);
+    mockWaveform.start();
+    countdownTimer.start();
     
 }
 
 void loop() {
     nexLoop(nex_listen_list);
-    
+}
+
+void generateWaveform(){
     if (counter>100){
         counter = 1;
     } else{
         counter++;
     }
-    
-    Serial1.print("n0.val=");
-    Serial1.print(counter);
-    Serial1.write(0xff);
-    Serial1.write(0xff);
-    Serial1.write(0xff);
-    delay(500);
-    
+    chart.addValue(0,counter);
+}
+
+void updateCountdownTimer(){
+    //Serial1.print("b2.txt=");
+    if (decaminutes==0&&minutes==0&&decaseconds==0&&seconds==0){
+        countdown.setText("ALARM");
+    } else{
+        if (seconds>0){
+            seconds--;
+        } else if (decaseconds>0){
+            decaseconds--;
+            seconds=9;
+        } else if (minutes>0){
+            minutes--;
+            decaseconds=5;
+            seconds=9;
+        } else if (decaminutes>0){
+            decaminutes--;
+            minutes=9;
+            decaseconds=5;
+            seconds=9;
+        }
+        char buf[10];
+        snprintf(buf, sizeof(buf), "%d%d:%d%d", decaminutes, minutes, decaseconds, seconds);
+        countdown.setText(buf);
+    }
+        // }
+        // Serial1.print("\"");
+        // if (minutes<10){Serial1.print("0");}
+        // Serial1.print(minutes);
+        // Serial1.print(":");
+        // if (seconds<10){Serial1.print("0");}
+        // Serial1.print(seconds);
+        // Serial1.print("\"");
+        // Serial1.write(0xff);
+        // Serial1.write(0xff);
+        // Serial1.write(0xff);
 }
